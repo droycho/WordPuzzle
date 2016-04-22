626c56645ba7d9ae0a23f5bcca4e177b1c5a2d48
DJ Roychoudhury <djroychoudhury@yahoo.com>
2016-04-22 12:14:23 -0700

    add integration testing for front-end. tests pass. all objectives met. now for style.

---
+++ src/test/java/ServerRule.java
@@ -0,0 +1,16 @@
+
+
+import org.junit.rules.ExternalResource;
+import spark.Spark;
+
+public class ServerRule extends ExternalResource {
+
+  protected void before() {
+    String[] args = {};
+    WordPuzzle.main(args);
+   }
+
+  protected void after() {
+    Spark.stop();
+  }
+}
