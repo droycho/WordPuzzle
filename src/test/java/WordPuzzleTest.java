b8f40627ccfb9853ca44cadddc099ffdae688990
DJ Roychoudhury <djroychoudhury@yahoo.com>
2016-05-02 16:34:48 -0700

    fixed test.

--- src/test/java/WordPuzzleTest.java
+++ src/test/java/WordPuzzleTest.java
@@ -9,35 +9,35 @@ public class WordPuzzleTest {
     WordPuzzle phrase = new WordPuzzle();
     String expected = new String();
     expected = "-nd";
-    assertEquals(expected, WordPuzzle.userPhrase("-nd"));
+    assertEquals(expected, WordPuzzle.userPhrase("and"));
   }
   @Test
     public void isUserPhrase_forReplaceLetterE_True() {
     WordPuzzle phrase = new WordPuzzle();
     String expected = new String();
     expected = "--k";
-    assertEquals(expected, WordPuzzle.userPhrase("--k"));
+    assertEquals(expected, WordPuzzle.userPhrase("eek"));
   }
   @Test
     public void isUserPhrase_forReplaceLetterI_True() {
     WordPuzzle phrase = new WordPuzzle();
     String expected = new String();
     expected = "-t";
-    assertEquals(expected, WordPuzzle.userPhrase("-t"));
+    assertEquals(expected, WordPuzzle.userPhrase("it"));
   }
   @Test
     public void isUserPhrase_forReplaceLetterO_True() {
     WordPuzzle phrase = new WordPuzzle();
     String expected = new String();
     expected = "-n";
-    assertEquals(expected, WordPuzzle.userPhrase("-n"));
+    assertEquals(expected, WordPuzzle.userPhrase("on"));
   }
   @Test
     public void isUserPhrase_forReplaceLetterU_True() {
     WordPuzzle phrase = new WordPuzzle();
     String expected = new String();
     expected = "-gly";
-    assertEquals(expected, WordPuzzle.userPhrase("-gly"));
+    assertEquals(expected, WordPuzzle.userPhrase("ugly"));
   }
   @Test
     public void isUserPhrase_forReplaceAllVowels_True() {
